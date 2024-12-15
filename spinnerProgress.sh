063047835f7a55e17a127e7d1c1297f70dca36fe
Anthony <33938805+Toroyteach@users.noreply.github.com>
2024-12-15 09:56:58 +0300

    Update spinnerProgress.sh

--- spinnerProgress.sh
+++ spinnerProgress.sh
@@ -5,20 +5,33 @@
 ## spinner like progress or any kind of progress to 
 ## to have something like a feedback to the user
 
-## function to run the spinner animation in a while loop
+# Start time
+start_time=$(date +%s)
+
+echo "Initialize Error Log:" >"$ERROR_LOG"
+echo "Initialize Duplicate Logs:" >"$DUPLICATE_LOG"
+echo "Initialize Other Error Logs:" >"$OTHER_ERROR_LOG"
+
+echo -e "\nStarted at: $(date -r $start_time '+%Y-%m-%d %H:%M:%S')\n"
+
 spin() {
-    spinner="/|\\-/|\\-"
+    local spinner="/|\\-/|\\-"
+    local start_time=$(date +%s)
     while :; do
         for i in $(seq 0 7); do
-            echo -n "${spinner:$i:1}"
-            echo -en "\010"
+            local current_time=$(date +%s)
+            local elapsed_time=$((current_time - start_time))
+            local hours=$((elapsed_time / 3600))
+            local minutes=$(((elapsed_time % 3600) / 60))
+            local seconds=$((elapsed_time % 60))
+
+            printf "\r%s %02d:%02d:%02d" "${spinner:$i:1}" $hours $minutes $seconds
             sleep 1
         done
     done
 }
 
-echo "About to make a slow web call..."
-
+tput civis
 # Start the Spinner:
 spin &
 # Make a note of its Process ID (PID):
@@ -29,6 +42,11 @@ trap "kill -9 $SPIN_PID" $(seq 0 15)
 ## make a web call or anything that will take time to finish
 sleep 10
 
-echo "Finished."
 
-kill -9 $SPIN_PID
+##Return the cursor and others
+tput cnorm
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+echo -e "\n\nFinished at: $(date -r $end_time '+%Y-%m-%d %H:%M:%S')"
+echo -e "\nElapsed time: $(printf "%02d:%02d:%02d\n" $((elapsed_time / 3600)) $(((elapsed_time % 3600) / 60)) $((elapsed_time % 60)))"
